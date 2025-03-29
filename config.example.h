c19ceb02dddbf0f6c453fa98416ecd07c405673e
Bartosz Poznananiak <bartek.poznaniak@gmail.com>
2025-03-29 13:17:09 +0100

    Usunięcie wrażliwych danych i dodanie bezpiecznej konfiguracji

---
+++ config.example.h
@@ -0,0 +1,9 @@
+#ifndef CONFIG_EXAMPLE_H
+#define CONFIG_EXAMPLE_H
+
+// Przykładowa konfiguracja WiFi
+// Skopiuj ten plik do config.h i uzupełnij własne dane
+#define WIFI_SSID "NAZWA_SIECI"
+#define WIFI_PASSWORD "HASLO_SIECI"
+
+#endif
